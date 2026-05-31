0ca2f6724601a28bf655b8bfb9c74b731317b003
iroha <y.koba.h1071@outlook.jp>
2026-05-31 22:33:19 +0900

    メンションすると返信を投稿するBotを作成

--- app.js
+++ app.js
@@ -1,52 +1,37 @@
-import { App } from '@slack/bolt';
+import pkg from "@slack/bolt";
+const { App, AwsLambdaReceiver } = pkg;
 
-/**
- * This sample Slack application uses Socket Mode.
- * For the companion getting started setup guide, see:
- * https://docs.slack.dev/tools/bolt-js/getting-started/
- */
+const awsLambdaReceiver = new AwsLambdaReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
 
 // Initializes your app with your bot token and app token
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN
+  receiver: awsLambdaReceiver,
 });
 
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ],
-    text: `Hey there <@${message.user}>!`
-  });
+const keywordResponses = [{ keyword: "こんにちは", reply: "こんにちは" }];
+
+app.event("app_mention", async ({ event, say }) => {
+  const text = event.text;
+  const matched = keywordResponses.find(({ keyword }) =>
+    text.includes(keyword),
+  );
+
+  if (matched) {
+    await say({
+      text: `@${event.user} ${matched.reply}`,
+      thread_ts: event.thread_ts ?? event.ts,
+    });
+  } else {
+    await say({
+      text: `<@${event.user}>`,
+    });
+  }
 });
 
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
-});
-
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
-
-  app.logger.info('⚡️ Bolt app is running!');
-})();
+export const handler = async (event, context, callback) => {
+  const lambdaHandler = await awsLambdaReceiver.start();
+  return lambdaHandler(event, context, callback);
+};
